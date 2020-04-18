42cd94da954e20d8ac10cc922071c1f8239ee6b1
James Geib <jpgeib2@gmail.com>
2020-04-18 11:38:10 -0700

    changed file structure/building footer

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Navbar from './../Navbar';
+import Navbar from '../../containers/Navbar';
 import LandingPage from "../../pages/LandingPage";
 import Chat from "../../pages/Chat";
 
