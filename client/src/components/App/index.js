b72ce2b0f8355ecfa9651732a8477440348e4dec
Paul Han <paul.myung.han@gmail.com>
2020-04-18 12:38:18 -0700

    set up socket and got response

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './../Navbar';
 import LandingPage from "../../pages/LandingPage";
 import Chat from "../../pages/Chat";
+import io from "socket.io-client"
+
+const socket = io();
+
+socket.emit("connection", {name:"hello"})
 
 const App = props => (
   <Router>
--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 require('dotenv').config()
 const express = require('express');
-
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const routes = require('./routes');
 
 
+//for production only
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('./client/build'));
 }
 
+//middlewares 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Declare routes right here.
-const routes = require('./routes');
 
 // Prepend / to any route declared inside of routes
 app.use(routes);
 
-app.listen(PORT, () => console.log('Port started on port: ' + PORT));
+//socket.io
+io.on("connection", socket => {
+    console.log("New client connected");
+    socket.on("connection", data => {
+        console.log(data);
+    })
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    })
+})
+
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log('Port started on port: ' + PORT));
+
+//psuedo code
+
+// users
+//     - multiple messages
+//     - multiple chatrooms
+// messages
+//     - single user
+//     - single chatroom
+// chatrooms
+//     - multiple users
+//     - multiple messages
+
+
+// login with username
+// start with 1 chatroom 
