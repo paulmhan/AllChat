19bfcbae96ca1c2ac5c31c20517644efed1d5117
James Geib <jpgeib2@gmail.com>
2020-04-18 12:05:31 -0700

    finished building footer and also created the 'Get Started' button

--- client/src/pages/Chat/index.js
+++ client/src/pages/Chat/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from "../../containers/Navbar/index";
+import Footer from "../../components/Footer/index"; 
 
 class Chat extends Component {
     render() {
         return (
             <div>
+                <Navbar/>
                 <h1>*Insert Chat Room Here*</h1>
+                <Footer/>
             </div>
         )
 
--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from "../../containers/Navbar/index";
+import Footer from "../../components/Footer/index";
+import GetStartedBtn from "../../components/GetStartedBtn/index";
 
 class LandingPage extends Component {
     render() {
         return (
             <div>
+                <Navbar/>
                 <h1>Welcome to AllChat!</h1>
+                <GetStartedBtn/>
+                <Footer/>
             </div>
         )
 
