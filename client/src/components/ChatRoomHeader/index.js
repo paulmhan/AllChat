775c6980e4f29cce2f90a641cc8793e8b1f4c503
Paul Han <paul.myung.han@gmail.com>
2020-04-20 19:44:45 -0700

    added dummy text for landing body text

--- client/src/components/ChatRoomHeader/index.js
+++ client/src/components/ChatRoomHeader/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import LeaveBtn from "./../LeaveBtn"
+
 
 const ChatRoomHeader = props => {
     return(
         <div>
-            <h1>AllChat</h1>
-            <LeaveBtn />
         </div>
     )
 }
