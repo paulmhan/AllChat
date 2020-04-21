8688adc5a22bb4e4354b2de55e2ed8d5c4cc9bd1
James Geib <jpgeib2@gmail.com>
2020-04-20 21:06:59 -0700

    possibly fixed the sidebar, going to test it out

--- client/src/components/RoomName/index.js
+++ client/src/components/RoomName/index.js
@@ -3,12 +3,7 @@ import { Segment } from "semantic-ui-react";
 
 const RoomName = props => {
     return(
-    <Segment.Group>
         <Segment>Chatroom 1</Segment>
-        <Segment>Chatroom 2</Segment>
-        <Segment>Chatroom 3</Segment>
-        <Segment>Chatroom 4</Segment>
-    </Segment.Group>
     );
 };
 
--- client/src/components/UsersList/index.js
+++ client/src/components/UsersList/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Segment } from "semantic-ui-react";
 
 const UserList = props => {
-    return(
-    <Segment.Group>
-        <Segment>User 1</Segment>
-        <Segment>User 2</Segment>
-        <Segment>User 3</Segment>
-        <Segment>User 4</Segment>
-    </Segment.Group>
+    return (
+        <div>
+            <Segment>User 1</Segment>
+            <Segment>User 2</Segment>
+            <Segment>User 3</Segment>
+            <Segment>User 4</Segment>
+        </div>
     );
 }
 
