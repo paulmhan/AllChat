3b9ca97abb695f0a73133992e66cdee007583b49
James Geib <jpgeib2@gmail.com>
2020-04-20 19:07:14 -0700

    finished creating the dropdown menu, also has a search functionality for items in the list

--- client/src/components/RoomDropdown/index.js
+++ client/src/components/RoomDropdown/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Dropdown } from "semantic-ui-react";
+
+const chatRoomOptions = [
+    {key: "ch1", value:"ch1", text: "Chatroom 1"},
+    {key: "ch2", value:"ch2", text: "Chatroom 2"},
+    {key: "ch3", value:"ch3", text: "Chatroom 3"}
+]
 
 const RoomDropdown = props => {
     return(
-        <div>
-
-        </div>
+        <Dropdown 
+            placeholder="Select a chatroom"
+            fluid
+            search
+            selection
+            options={chatRoomOptions}
+        />
     )
 }
 
