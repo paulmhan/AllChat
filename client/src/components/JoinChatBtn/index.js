9114a219fc5ba50e88d8e60a245061e9798b4274
suneethaburla <suneetha@gmail.com>
2020-04-20 19:26:23 -0700

    Added chatheader and sign up button on signup page

--- client/src/components/JoinChatBtn/index.js
+++ client/src/components/JoinChatBtn/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 const JoinChatBtn = props => {
     return(
-        <div>
-
+       <div>
+            <Button>Join Chat!</Button>
         </div>
-    )
+    );
 }
 
 export default JoinChatBtn;
