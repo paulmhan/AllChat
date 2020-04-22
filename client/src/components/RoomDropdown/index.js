c0239e86efb1a7e0c3c15fe136a5fb7457e3ad2c
Paul Han <paul.myung.han@gmail.com>
2020-04-22 12:40:34 -0700

    changed dropdown menu to input and captured state to progress with functionality

--- client/src/components/RoomDropdown/index.js
+++ client/src/components/RoomDropdown/index.js
@@ -1,30 +1,44 @@
+// import React, { Component } from "react";
+// import { Dropdown } from "semantic-ui-react";
+
+// class RoomDropdown extends Component {
+//     state = {
+//         value: "",
+//     }
+
+//     render(){
+//         const chatRoomOptions = [
+//             {key: "ch1", value:"ch1", text: "Chatroom 1"},
+//             {key: "ch2", value:"ch2", text: "Chatroom 2"},
+//             {key: "ch3", value:"ch3", text: "Chatroom 3"},
+//             {key: "ch4", value:"ch4", text: "Chatroom 4"}
+//         ]  
+//         const {value} = this.state;
+
+//         return(
+//             <Dropdown 
+//                 placeholder="Select a chatroom"
+//                 fluid
+//                 search
+//                 selection
+//                 options={chatRoomOptions}
+//                 value={value}
+//             />
+//         )
+
+        
+//     }
+// }
+    
+// export default RoomDropdown;
+
 import React, { Component } from "react";
-import { Dropdown } from "semantic-ui-react";
+import { Input } from "semantic-ui-react";
 
 class RoomDropdown extends Component {
-    state = {
-        value: "",
-    }
 
     render(){
-        const chatRoomOptions = [
-            {key: "ch1", value:"ch1", text: "Chatroom 1"},
-            {key: "ch2", value:"ch2", text: "Chatroom 2"},
-            {key: "ch3", value:"ch3", text: "Chatroom 3"},
-            {key: "ch4", value:"ch4", text: "Chatroom 4"}
-        ]  
-        const {value} = this.state;
-
-        return(
-            <Dropdown 
-                placeholder="Select a chatroom"
-                fluid
-                search
-                selection
-                options={chatRoomOptions}
-                value={value}
-            />
-        )
+        return <Input onChange={this.props.getRoom} placeholder="What is the Name of the Room You Would Like to Create or Join?"/>
 
         
     }
--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-// import Footer from "../../components/Footer";
-import SignUpHeader from "../../components/SignUpHeader"
-import UsernameInput from "../../components/UsernameInput"
-import RoomDropdown from "../../components/RoomDropdown"
-import JoinChatBtn from "../../components/JoinChatBtn"
+import SignUpHeader from "../../components/SignUpHeader";
+import UsernameInput from "../../components/UsernameInput";
+import RoomDropdown from "../../components/RoomDropdown";
+import JoinChatBtn from "../../components/JoinChatBtn";
 
 class LandingPage extends Component {
     state = {
