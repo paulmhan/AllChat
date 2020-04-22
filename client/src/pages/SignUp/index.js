fb3b885b846a132137f8ce3f25bc2f9d2cad4162
Paul Han <paul.myung.han@gmail.com>
2020-04-22 13:12:15 -0700

    join button leads to chatroom with both name and room name

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import SignUpHeader from "../../components/SignUpHeader";
 import UsernameInput from "../../components/UsernameInput";
 import RoomDropdown from "../../components/RoomDropdown";
-import JoinChatBtn from "../../components/JoinChatBtn";
+// import JoinChatBtn from "../../components/JoinChatBtn";
 
 class LandingPage extends Component {
     state = {
@@ -10,33 +11,44 @@ class LandingPage extends Component {
         room: "",
     };
 
-    handleNameChange = (e) => {
+    handleNameChange = e => {
         const { value } = e.target;
         console.log(value);
         this.setState({ name: value });
     };
 
-    handleRoomChange = (e) => {
+    handleRoomChange = e => {
         const { value } = e.target;
         console.log(value);
         this.setState({ room: value });
     };
 
+    checkInputs = e => {
+        if(!this.state.name || !this.state.room){
+            e.preventDefault();
+            console.log("Need to input both name and room");
+        } else {
+            console.log("Welcome!");
+        }
+    }
+
     render() {
         return (
             <div>
                 <SignUpHeader />
-                <UsernameInput 
-                getName={this.handleNameChange} 
+                <UsernameInput
+                    getName={this.handleNameChange}
                 />
-                <RoomDropdown 
-                getRoom={this.handleRoomChange}
+                <RoomDropdown
+                    getRoom={this.handleRoomChange}
                 />
-                <JoinChatBtn />
+                <Link onClick={this.checkInputs} to={`/chat?name=${this.state.name}&room=${this.state.room}`}>
+                    <button className="button mt-20" type="submit"> Join Chat Room! </button>
+                </Link>
             </div>
-        )
-
-    }
-}
-
+                )
+        
+            }
+        }
+        
 export default LandingPage;
