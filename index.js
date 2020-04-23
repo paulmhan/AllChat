fcb4aa7b54937030b8786d25e34a8cf4e83b4e62
Paul Han <paul.myung.han@gmail.com>
2020-04-22 19:58:43 -0700

    added lifecycle to capture name and room and used socket to send data to server

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(routes);
 //socket.io
 io.on("connection", socket => {
     console.log("New client connected.");
-    socket.on("connection", data => {
-        console.log(data);
+    socket.on("joinChat", ({ name, room }) => {
+        console.log(name,room);
     })
     socket.on("disconnect", () => {
         console.log("Client disconnected.");
