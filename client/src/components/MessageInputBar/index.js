fc2d4493c65f3b0ba9dcee5c1a7a181696e75b41
James Geib <jpgeib2@gmail.com>
2020-04-25 13:56:10 -0700

    minor change

--- client/src/components/MessageInputBar/index.js
+++ client/src/components/MessageInputBar/index.js
@@ -5,19 +5,19 @@ import { Input } from "semantic-ui-react";
 const MessageInputBar = props => {
     return (
         <div>
-           <Input
-                fluid
-                error={props.error}
-                value={props.message}
-                onChange={props.getMessage}
-                action={{
-                    color: "blue",
-                    labelPosition: "right",
-                    icon: "arrow circle up",
-                    content: "Send",
-                }}
-                placeholder={props.placeholder}
-            />
+           <Form >
+                <Form.Field
+                    error={props.error}
+                    value={props.message}
+                    required
+                    placeholder={props.placeholder}
+                    control='input'
+                    onChange={props.getMessage}
+                />
+            </Form>
+            <Button onClick={props.handleSend} primary type="submit">
+                Send
+            </Button>
         </div>
     )
 };
@@ -25,16 +25,18 @@ const MessageInputBar = props => {
 export default MessageInputBar;
 
 
-// {/* <Form >
-//                 <Form.Field
-//                     error={props.error}
-//                     value={props.message}
-//                     required
-//                     placeholder={props.placeholder}
-//                     control='input'
-//                     onChange={props.getMessage}
-//                 />
-//             </Form>
-//             <Button onClick={props.handleSend} primary type="submit">
-//                 Send
-//             </Button> */}
+
+
+// {/* <Input
+//                 fluid
+//                 error={props.error}
+//                 value={props.message}
+//                 onChange={props.getMessage}
+//                 action={{
+//                     color: "blue",
+//                     labelPosition: "right",
+//                     icon: "arrow circle up",
+//                     content: "Send",
+//                 }}
+//                 placeholder={props.placeholder}
+//             /> */}
